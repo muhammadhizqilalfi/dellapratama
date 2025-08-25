cb9cb511f34c7b31b8ebb8dcfae258ccc4f61e84
Muhammad Hizqil Alfi <Hizqilhizqil308@gmail.com>
2025-08-26 06:25:37 +0700

    Update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -39,7 +39,7 @@ export default function Footer() {
             Portofolio
           </Link>
           <Link href="contact" className="hover:text-blue-900">
-            Contact Us
+            Kontak
           </Link>
         </nav>
 
